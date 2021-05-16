e53e43bcba9f7a7507ea68f228a8916a7cadfe98
Bruno <delattreb@gmail.com>
2021-05-16 17:05:48 +0200

    Fix

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var express_1 = __importDefault(require("express"));
 var app = express_1.default();
 var port = 3000;
 var user = { name: "Bruno", age: 45, sex: "M" };
-var p1 = { id: 0, name: "Bruno", team: 56 };
+var p1 = { id: 0, name: "Bruno", team: 12 };
 app.get("/", function (req, res) {
-    res.send("Hi there Salut");
+    res.send("Hi there");
 });
 app.get("/user", function (req, res) {
     res.send(user);
@@ -17,6 +17,11 @@ app.get("/user", function (req, res) {
 app.get("/player", function (req, res) {
     res.send(p1);
 });
-app.listen(port, function () {
-    return console.log("server is listening on " + port);
-});
+app.listen(port);
+console.log("Server listen on port: " + port);
+app.on("error", onError);
+function onError(error) {
+    if (error.syscall !== "listen") {
+        throw error;
+    }
+}
